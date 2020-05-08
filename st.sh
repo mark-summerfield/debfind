d65f402380d009720222bf007ee7c3a56423cbcb
mark-summerfield <mark@qtrac.eu>
2020-05-08 23:22:42 +0100

    minor UI fix + lint

--- st.sh
+++ st.sh
@@ -6,14 +6,14 @@ flake . \
 vulture --exclude ddebfind,gravitatewx . \
     | grep -v Model.py.*unused.*allSections \
     | grep -v Model.py.*unused.*dumpIndexes \
-    | grep -v HelpForm.py.*Unused.attribute...i \
-    | grep -v WindowActions.py.*Unused.attribute..Name \
-    | grep -v WindowActions.py.*Unused.attribute..Version \
-    | grep -v WindowActions.py.*Unused.attribute..Description \
-    | grep -v WindowActions.py.*Unused.attribute..Copyright \
-    | grep -v WindowActions.py.*Unused.attribute..WebSite \
-    | grep -v Window.py.*Unused.class..Window \
-    | grep -v Window.py.*Unused.attribute..Title \
-    | grep -v Window.py.*Unused.attribute..MinSize \
-    | grep -v Window.py.*Unused.attribute..Selection
+    | grep -v HelpForm.py.*[Uu]nused.attribute...i \
+    | grep -v WindowActions.py.*[Uu]nused.attribute..Name \
+    | grep -v WindowActions.py.*[Uu]nused.attribute..Version \
+    | grep -v WindowActions.py.*[Uu]nused.attribute..Description \
+    | grep -v WindowActions.py.*[Uu]nused.attribute..Copyright \
+    | grep -v WindowActions.py.*[Uu]nused.attribute..WebSite \
+    | grep -v Window.py.*[Uu]nused.class..Window \
+    | grep -v Window.py.*[Uu]nused.attribute..Title \
+    | grep -v Window.py.*[Uu]nused.attribute..MinSize \
+    | grep -v Window.py.*[Uu]nused.attribute..Selection
 git status
